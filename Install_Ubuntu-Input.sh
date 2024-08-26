ed14f25e9212bedaa0bceaf14660e07d83caded9
Rizky Fachrurozy <81737766+blackabed@users.noreply.github.com>
2024-08-26 10:26:36 +0700

    Create Install_Ubuntu-Input.sh

---
+++ Install_Ubuntu-Input.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create directory
+mkdir -p sent1
+cd sent1
+
+# Download Sentinel Agent
+wget https://github.com/samphoerna/Sentinel-Repo/raw/main/Linux/SentinelAgent_linux_x86_64_v24_2_1_8.deb
+
+# Prompt user for SiteToken
+read -p "Please enter the SiteToken: " site_token
+
+# Install Sentinel Agent
+sudo dpkg -i SentinelAgent_linux_x86_64_v24_2_1_8.deb
+
+# Set the management token with user input
+/opt/sentinelone/bin/sentinelctl management token set "$site_token"
+
+# Start the Sentinel agent
+/opt/sentinelone/bin/sentinelctl control start

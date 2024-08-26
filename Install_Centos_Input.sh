bbf4d16b56e91fc8105e3ef3ddf2e3571255edc9
Rizky Fachrurozy <81737766+blackabed@users.noreply.github.com>
2024-08-26 10:28:04 +0700

    Create Install_Centos_Input.sh

---
+++ Install_Centos_Input.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create directory
+mkdir -p sent1
+cd sent1
+
+# Download Sentinel Agent
+wget https://github.com/samphoerna/Sentinel-Repo/raw/main/Linux/SentinelAgent_linux_x86_64_v24_2_1_8.rpm
+
+# Prompt user for SiteToken
+read -p "Please enter the SiteToken: " site_token
+
+# Install Sentinel Agent
+sudo rpm -i --nodigest SentinelAgent_linux_x86_64_v24_2_1_8.rpm
+
+# Set the management token with user input
+/opt/sentinelone/bin/sentinelctl management token set "$site_token"
+
+# Start the Sentinel agent
+/opt/sentinelone/bin/sentinelctl control start

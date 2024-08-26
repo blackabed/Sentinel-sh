cd6dfd498e880a9cf7e9b95c8c6f7a01fda29136
Rizky Fachrurozy <81737766+blackabed@users.noreply.github.com>
2024-08-26 10:34:27 +0700

    Update Install_Ubuntu-Input.sh

--- Install_Ubuntu-Input.sh
+++ Install_Ubuntu-Input.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Prompt user for SiteToken
+read -p "Please enter the SiteToken: " site_token
+
 # Create directory
 mkdir -p sent1
 cd sent1
 
 # Download Sentinel Agent
 wget https://github.com/samphoerna/Sentinel-Repo/raw/main/Linux/SentinelAgent_linux_x86_64_v24_2_1_8.deb
-
-# Prompt user for SiteToken
-read -p "Please enter the SiteToken: " site_token
 
 # Install Sentinel Agent
 sudo dpkg -i SentinelAgent_linux_x86_64_v24_2_1_8.deb

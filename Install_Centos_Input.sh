0025f643d726e02269e5f8dfe2e23025cc76aa0d
Rizky Fachrurozy <81737766+blackabed@users.noreply.github.com>
2024-08-26 10:33:42 +0700

    Update Install_Centos_Input.sh

--- Install_Centos_Input.sh
+++ Install_Centos_Input.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+# Prompt user for SiteToken
+read -p "Please enter the SiteToken: " site_token
+
 # Create directory
 mkdir -p sent1
 cd sent1
 
 # Download Sentinel Agent
 wget https://github.com/samphoerna/Sentinel-Repo/raw/main/Linux/SentinelAgent_linux_x86_64_v24_2_1_8.rpm
-
-# Prompt user for SiteToken
-read -p "Please enter the SiteToken: " site_token
 
 # Install Sentinel Agent
 sudo rpm -i --nodigest SentinelAgent_linux_x86_64_v24_2_1_8.rpm
